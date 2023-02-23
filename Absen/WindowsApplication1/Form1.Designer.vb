3068663c48b19d242ea5472c8f9e0791bba814c8
Gusase_ <bs1gusesa@gmail.com>
2023-02-23 10:51:55 +0700

    Add files via upload

---
+++ Absen/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker
+        Me.btnsimpan = New System.Windows.Forms.Button
+        Me.btnupdate = New System.Windows.Forms.Button
+        Me.btnrefresh = New System.Windows.Forms.Button
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.btndelete = New System.Windows.Forms.Button
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(-1, -1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(816, 138)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FORM ABSENSI  KELAS" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "XI RPL 2"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 141)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama Lengkap :"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(18, 208)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 23)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Kelas :"
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(18, 285)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(121, 19)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Mata Pelajaran :"
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(18, 360)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 23)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Tanggal :"
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(18, 436)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(100, 23)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Keterangan :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(21, 163)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(230, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(21, 307)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(230, 21)
+        Me.ComboBox2.TabIndex = 8
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(21, 462)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(230, 21)
+        Me.ComboBox3.TabIndex = 9
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(295, 163)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(487, 318)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(21, 387)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(230, 20)
+        Me.DateTimePicker1.TabIndex = 11
+        '
+        'btnsimpan
+        '
+        Me.btnsimpan.Location = New System.Drawing.Point(21, 502)
+        Me.btnsimpan.Name = "btnsimpan"
+        Me.btnsimpan.Size = New System.Drawing.Size(77, 32)
+        Me.btnsimpan.TabIndex = 12
+        Me.btnsimpan.Text = "Simpan"
+        Me.btnsimpan.UseVisualStyleBackColor = True
+        '
+        'btnupdate
+        '
+        Me.btnupdate.Location = New System.Drawing.Point(104, 502)
+        Me.btnupdate.Name = "btnupdate"
+        Me.btnupdate.Size = New System.Drawing.Size(82, 32)
+        Me.btnupdate.TabIndex = 13
+        Me.btnupdate.Text = "Update"
+        Me.btnupdate.UseVisualStyleBackColor = True
+        '
+        'btnrefresh
+        '
+        Me.btnrefresh.Location = New System.Drawing.Point(192, 502)
+        Me.btnrefresh.Name = "btnrefresh"
+        Me.btnrefresh.Size = New System.Drawing.Size(80, 32)
+        Me.btnrefresh.TabIndex = 14
+        Me.btnrefresh.Text = "Refresh"
+        Me.btnrefresh.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(21, 234)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(230, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'btndelete
+        '
+        Me.btndelete.Location = New System.Drawing.Point(279, 502)
+        Me.btndelete.Name = "btndelete"
+        Me.btndelete.Size = New System.Drawing.Size(80, 32)
+        Me.btndelete.TabIndex = 15
+        Me.btndelete.Text = "Delete"
+        Me.btndelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(811, 600)
+        Me.Controls.Add(Me.btndelete)
+        Me.Controls.Add(Me.btnrefresh)
+        Me.Controls.Add(Me.btnupdate)
+        Me.Controls.Add(Me.btnsimpan)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnsimpan As System.Windows.Forms.Button
+    Friend WithEvents btnupdate As System.Windows.Forms.Button
+    Friend WithEvents btnrefresh As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents btndelete As System.Windows.Forms.Button
+
+End Class
---
+++ Absen/WindowsApplication1/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    'membuat objek koneksi dan adapter
+    Dim connString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=dbabsen.accdb;"
+    Dim conn As New OleDbConnection(connString)
+    Dim adapter As New OleDbDataAdapter()
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'memuat data dari database dan menampilkan pada datagridview
+        Dim ds As New DataSet()
+        adapter.SelectCommand = New OleDbCommand("SELECT * FROM Absensi", conn)
+        adapter.Fill(ds, "Absensi")
+        DataGridView1.DataSource = ds.Tables("Absensi")
+
+        'menambahkan pilihan pada combobox1
+        ComboBox1.Items.Add("X")
+        ComboBox1.Items.Add("XI")
+        ComboBox1.Items.Add("XII")
+
+        'menambahkan pilihan pada combobox3
+        ComboBox3.Items.Add("Hadir")
+        ComboBox3.Items.Add("Izin")
+        ComboBox3.Items.Add("Sakit")
+        ComboBox3.Items.Add("Alpa")
+    End Sub
+
+    Private Sub btnsimpan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnsimpan.Click
+        'menambah data ke database
+        Dim cmd As New OleDbCommand("INSERT INTO absensi (Nama, Kelas, Mata_Pelajaran, Keterangan, Tanggal) VALUES (@Nama, @Kelas, @Mata_Pelajaran, @Keterangan, @Tanggal)", conn)
+        cmd.Parameters.AddWithValue("@Nama", TextBox1.Text)
+        cmd.Parameters.AddWithValue("@Kelas", ComboBox1.Text)
+        cmd.Parameters.AddWithValue("@Mata_Pelajaran", ComboBox2.Text)
+        cmd.Parameters.AddWithValue("@Keterangan", ComboBox3.Text)
+        cmd.Parameters.AddWithValue("@Tanggal", DateTimePicker1.Value)
+        conn.Open()
+        cmd.ExecuteNonQuery()
+        conn.Close()
+
+        MsgBox("Data telah ditambahkan.")
+        RefreshData()
+    End Sub
+
+    Private Sub btnupdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnupdate.Click
+        'mengupdate data di database
+        Dim cmd As New OleDbCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "UPDATE Absensi SET Nama=@nama, Kelas=@kelas, Mata_Pelajaran=@mapel, Keterangan=@hadir, Tanggal=@waktu WHERE id=@id"
+        cmd.Parameters.AddWithValue("@nama", TextBox1.Text)
+        cmd.Parameters.AddWithValue("@kelas", ComboBox1.Text)
+        cmd.Parameters.AddWithValue("@mapel", ComboBox2.Text)
+        cmd.Parameters.AddWithValue("@hadir", ComboBox3.Text)
+        cmd.Parameters.AddWithValue("@waktu", DateTimePicker1.Value)
+        cmd.Parameters.AddWithValue("@id", DataGridView1.CurrentRow.Cells("id").Value.ToString())
+        cmd.Connection = conn
+
+        conn.Open()
+        cmd.ExecuteReader()
+        conn.Close()
+
+        MsgBox("Data telah diupdate.")
+        RefreshData()
+    End Sub
+
+    Private Sub btnrefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnrefresh.Click
+        'memuat data dari database dan menampilkan pada datagridview
+        Dim ds As New DataSet()
+        adapter.SelectCommand = New OleDbCommand("SELECT * FROM Absensi", conn)
+        Dim builder As New OleDbCommandBuilder(adapter)
+        adapter.Fill(ds, "Absensi")
+        DataGridView1.DataSource = ds.Tables("Absensi")
+
+        'mengosongkan TextBox, ComboBox, dan DateTimePicker
+        TextBox1.Text = ""
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+        ComboBox3.Text = ""
+        DateTimePicker1.Value = Date.Now
+
+        MsgBox("Data Telah di Refresh")
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        'menampilkan data ke TextBox, ComboBox, DateTimePicker
+        TextBox1.Text = DataGridView1.CurrentRow.Cells("Nama").Value.ToString()
+        ComboBox1.Text = DataGridView1.CurrentRow.Cells("Kelas").Value.ToString()
+        ComboBox2.Text = DataGridView1.CurrentRow.Cells("Mata_Pelajaran").Value.ToString()
+        ComboBox3.Text = DataGridView1.CurrentRow.Cells("Keterangan").Value.ToString()
+    End Sub
+
+    Private Sub RefreshData()
+        'memuat data dari database dan menampilkan pada datagridview
+        Dim ds As New DataSet()
+        adapter.SelectCommand = New OleDbCommand("SELECT * FROM Absensi", conn)
+        Dim builder As New OleDbCommandBuilder(adapter)
+        adapter.Fill(ds, "Absensi")
+        DataGridView1.DataSource = ds.Tables("Absensi")
+
+        'membersihkan kolom input
+        TextBox1.Text = ""
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+        ComboBox3.Text = ""
+        DateTimePicker1.Value = DateTime.Now
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "X" Then
+            'jika kelas X dipilih, maka pilihan pada ComboBox mata pelajaran hanya IPA, IPS, dan KIMIA
+            ComboBox2.Items.Clear()
+            ComboBox2.Items.Add("IPA")
+            ComboBox2.Items.Add("IPS")
+            ComboBox2.Items.Add("KIMIA")
+        Else
+            'jika kelas selain X dipilih, maka pilihan pada ComboBox mata pelajaran adalah semua pilihan
+            ComboBox2.Items.Clear()
+            ComboBox2.Items.Add("IPA")
+            ComboBox2.Items.Add("IPS")
+            ComboBox2.Items.Add("FISIKA")
+            ComboBox2.Items.Add("KIMIA")
+        End If
+    End Sub
+
+    Private Sub btndelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndelete.Click
+        Dim cmd As New OleDbCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "DELETE FROM absensi WHERE id=@id"
+        cmd.Parameters.AddWithValue("@id", DataGridView1.CurrentRow.Cells("Nama").Value.ToString())
+        cmd.Connection = conn
+
+        conn.Open()
+        cmd.ExecuteNonQuery()
+        conn.Close()
+
+        MsgBox("Data telah dihapus.")
+        RefreshData()
+    End Sub
+End Class
+
